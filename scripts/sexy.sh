95706583fb21c99df50ecfdd7f6d8cc3b7c476d0
Marco Kellershoff <marco@kellershoff.net>
2024-03-21 08:58:02 +0100

    💄 Sexy rounded corners tmux pane

--- scripts/sexy.sh
+++ scripts/sexy.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 export LC_ALL=en_US.UTF-8
 
-current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-source $current_dir/utils.sh
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source $CURRENT_DIR/utils.sh
+
+CONFIG_NERDFONTS_RIGHT=$(get_option "@sexy-nerdfonts-right" )
+CONFIG_NERDFONTS_LEFT=$(get_option "@sexy-nerdfonts-right" █)
+
+tmux set -gw window-status-current-format "#[fg=magenta,bg=#2F343F,nobold,nounderscore,noitalics]$CONFIG_NERDFONTS_LEFT#[bg=magenta#,fg=#2F343F] #I #W #[fg=magenta,bg=#2F343F,nobold,nounderscore,noitalics]$CONFIG_NERDFONTS_RIGHT"
